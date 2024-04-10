e5e2eb3a05714efbc05240dbf22c30f682a89548
tanya-jp <t.djavaherpour@gmail.com>
2024-04-10 17:15:29 -0400

    Add data module docs

--- src/data.py
+++ src/data.py
@@ -1,14 +1,42 @@
+"""Module for loading and processing data.
 
-import gdown
-import tarfile
-import os
-from keras.datasets import cifar10
-from tensorflow.keras.utils import to_categorical
-from matplotlib import pyplot
-import numpy as np
+This module includes functions for loading and processing CIFAR 10 dataset
+used in our application. It provides utilities for loading,
+grayscaling, normalizing, flattiening, and shuffling train and test dataset.
+"""
 import random
+import os
+import tarfile
+import gdown
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import keras
 
 class Data():
+
+    """Class for loading and processing dataset.
+
+    This class provides functionalities to download, process, and prepare 
+    CIFAR 10 for the image classification tasks. 
+    It handles tasks such as downloading data, converting RGB 
+    images to grayscale, normalizing pixel values,
+    flattening image data, and shuffling the data for randomness.
+
+    Attributes:
+        train_data (list): List to store training data.
+        train_images (list): List to store training images.
+        test_data (list): List to store testing data.
+        test_images (list): List to store testing images.
+
+    Methods:
+        save_data: Downloads and extracts dataset from a given URL.
+        load_data: Loads the dataset into the class attributes.
+        rgb2gray: Converts RGB image data to grayscale.
+        prep_pixels: Normalizes pixel values in the dataset.
+        flat_data: Flattens the image data from 2D to 1D.
+        shuffle_data: Randomizes the order of the dataset.
+        get_dataset: Processes the dataset and returns the processed training and testing data.
+    """
+
 
     def __init__(self):
 
@@ -18,7 +46,10 @@ class Data():
         self.test_images = []
 
     def save_data(self):
-        # Download the file from Google Drive
+        """
+        Download the file from Google Drive and save it locally if it 
+        isn't already saved
+        """
         url = 'https://drive.google.com/uc?id=1Y1vgzPvMeVcXSxDfOlCVia7wsU7p8M6g'
         output = 'CIFAR10.tar.gz'
         unzipped = 'CIFAR10'
@@ -29,94 +60,162 @@ class Data():
         # Check if the file was downloaded successfully
         if not os.path.isfile(output):
             raise Exception("UnableToDownload")
-        else:
-            print("Dataset downloaded")
+
+        print("Dataset downloaded")
 
         # Extract the downloaded tar.gz file
         if not os.path.isfile(unzipped):
             with tarfile.open(output) as file:
                 file.extractall()
 
-    def load_data(self): 
+    def load_data(self):
+        """
+        Load train and test dataset, ecnod the labels 
+        and check if it is loaded completely.
+        """
         # Download dataset
-        self.train_data, self.test_data = cifar10.load_data()
-        (self.train_images, trainY) = self.train_data
-        (self.test_images, testY) = self.test_data
+        self.train_data, self.test_data = keras.datasets.cifar10.load_data()
+        (self.train_images, train_y) = self.train_data
+        (self.test_images, test_y) = self.test_data
 
         # one hot encode target values
-        trainY = to_categorical(trainY)
-        testY = to_categorical(testY)
+        train_y = keras.utils.to_categorical(train_y)
+        test_y = keras.utils.to_categorical(test_y)
 
         # Check if the file was downloaded successfully
-        if self.train_images.shape != (50000, 32, 32, 3) or trainY.shape != (50000, 10) or self.test_images.shape != (10000, 32, 32, 3) or testY.shape != (10000, 10):
+        if (self.train_images.shape != (50000, 32, 32, 3) or train_y.shape != (50000, 10) or
+            self.test_images.shape != (10000, 32, 32, 3) or test_y.shape != (10000, 10)):
             raise Exception("UnableToDownload")
-        
+        print("Downloaded")
+
+    def rgb2gray(self, images, input_image = False):
+        """
+        Convert RGB image data into grayscale.
+
+        This method is used to reduce the complexity of image data by converting 
+        RGB (Red, Green, Blue) images to grayscale. 
+        In a grayscale image, each pixel represents a shade of gray corresponding 
+        to the intensity of the original colors.
+
+        Parameters:
+        images (array): A numpy array representing one or more images in RGB format. 
+                        If a single image, it should be a 3D array 
+                        (height, width, color_channels).
+                        If multiple images, it should be a 4D array 
+                        (number_images, height, width, color_channels).
+        input_image (bool): A flag to indicate if the provided 'images' 
+                            parameter is a single image (True) or 
+                            a batch of images (False). Default is False.
+
+        Returns:
+        array: The transformed grayscale images. If 'images' was a 
+                single image, the return is a 2D array (height, width).
+                If 'images' was a batch of images, the return is a 3D 
+                array (number_images, height, width).
+        """
+        if input_image:
+            r, g, b = images[:, :, 0], images[:, :,1], images[:, :, 2]
         else:
-            print("Downloaded")
-
-    # Convert RGB data into grayscale in order to reduce complexity
-    def rgb2gray(self):
-        # Train images
-        r, g, b = self.train_images[:, :, :, 0], self.train_images[:, :, :,1], self.train_images[:, :, :, 2]
-        self.train_images = 0.2989 * r + 0.5870 * g + 0.1140 * b
-
-        # Test images
-        r, g, b = self.test_images[:, :, :, 0], self.test_images[:, :, :,1], self.test_images[:, :, :, 2]
-        self.test_images = 0.2989 * r + 0.5870 * g + 0.1140 * b
-
+            r, g, b = images[:, :, :, 0], images[:, :, :,1], images[:, :, :, 2]
+        images = 0.2989 * r + 0.5870 * g + 0.1140 * b
         print("Grayscaled")
 
-    # Scale pixels to change the range of data between 0 and 1
-    def prep_pixels(self):
-        # Convert from integers to floats
-        self.train_images = self.train_images.astype('float32')
-        self.test_images = self.test_images.astype('float32')
+        return images
+
+    def prep_pixels(self, images):
+        """
+        Normalize image pixel values to the [0, 1] range.
+
+        Converts image data to float32 and scales pixel values to range between 0 and 1 
+        for neural network compatibility.
+        
+        Parameters:
+        images (array): A numpy array of image data.
+
+        Returns:
+        array: Normalized image data.
+        """
+         # Convert from float64 to float32
+        images = images.astype('float32')
 
         # Normalize to range 0-1
-        self.train_images = self.train_images / 255.0
-        self.test_images = self.test_images / 255.0
+        images = images / 255.0
         print("Normalized")
+        return images
 
-    # Flats the 2D matrices to an 1D vector
-    def flat_data(self):
-        self.train_images = self.train_images.reshape(-1, 1024)
-        self.test_images = self.test_images.reshape(-1, 1024)
+    def flat_data(self, images):
+        """
+        Flatten 2D image matrices into 1D vectors.
+
+        Reshapes each image in the given array from a 2D matrix to a 1D vector.
+
+        Parameters:
+        images (array): A numpy array of 2D image matrices.
+
+        Returns:
+        array: A numpy array of 1D image vectors.
+        """
+        images = images.reshape(-1, 1024)
 
         print("flatten")
+        return images
 
-    def shuffle_data(self):
+    def shuffle_data(self, data, images):
+        """
+        Shuffle image data and labels together.
+
+        Combines images with their corresponding labels, one-hot encodes the labels, 
+        and then shuffles the combined data for randomness.
+
+        Parameters:
+        data (tuple): A tuple containing two elements: images and labels.
+        images (array): A numpy array of images.
+
+        Returns:
+        list: A list of tuples, where each tuple contains a shuffled image and its label.
+        """
         # make data sets by appending data and labels
-        (raw_train_images, train_labels) = self.train_data
-        (raw_test_images, test_labels) = self.test_data
+        ( _, labels) = data
 
-        self.train_data = []
-        self.test_data = []
+        data = []
 
         # one hot encode target values
-        train_labels = to_categorical(train_labels)
-        test_labels = to_categorical(test_labels)
+        labels = keras.utils.to_categorical(labels)
 
-        for i in range(len(self.train_images)):
-            self.train_data.append((self.train_images[i].reshape(1024, 1), train_labels[i].reshape(10,1)))
+        for i, image in enumerate(images):
+            data.append((image.reshape(1024, 1), labels[i].reshape(10,1)))
 
-        for i in range(len(self.test_images)):
-            self.test_data.append((self.test_images[i].reshape(1024, 1), test_labels[i].reshape(10,1)))
 
         # shuffle data matrix
-        random.shuffle(self.train_data)
-        random.shuffle(self.test_data)
+        random.shuffle(data)
 
         print("shuffled")
 
-    def get_dataset(self):
-        self.load_data()
-        self.rgb2gray()
-        self.prep_pixels()
-        self.flat_data()
-        self.shuffle_data()
-        return self.train_data, self.test_data
+        return data
 
-if __name__ == '__main__': 
+
+    def get_dataset(self):
+        """
+        Process and retrieve the training and testing datasets.
+
+        This method orchestrates various data processing steps including loading data, 
+        converting to grayscale, normalizing pixel values, flattening images, and shuffling data.
+        It ensures the dataset is properly formatted and prepared for use in machine learning
+        models.
+
+        Returns:
+        tuple: A tuple containing processed training and testing datasets.
+    """
+        self.load_data()
+        self.train_images = self.rgb2gray(self.train_images)
+        self.test_images = self.rgb2gray(self.test_images)
+        self.train_images = self.prep_pixels(self.train_images)
+        self.test_images = self.prep_pixels(self.test_images)
+        self.train_images = self.flat_data(self.train_images)
+        self.test_images = self.flat_data(self.test_images)
+        self.train_data = self.shuffle_data(self.train_data, self.train_images)
+        self.test_images = self.shuffle_data(self.test_data, self.test_images)
+        return self.train_data, self.test_data
+if __name__ == '__main__':
     d = Data()
     d.get_dataset()
-
